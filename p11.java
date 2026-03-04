e21bb5c2fcf87d15fc8926c8d25092375962a090
madhan gowda B H <mmadhangowdabh@gmail.com>
2026-03-04 10:23:48 +0530

    java 11th program added ,

---
+++ p11.java
@@ -0,0 +1,16 @@
+// Factorial Calculation in Java 
+
+public class p11 {
+    public static void main(String[] args) {
+        int number = 5;
+        int factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static int calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * calculateFactorial(n - 1);
+    }
+}
